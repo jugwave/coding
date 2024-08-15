2b088a3ae66e5945305dc4d6d1f580103dab4104
jugwave <bernadettegopela.c@gmail.com>
2024-08-15 23:54:06 +0800

    Update sesamesyrup.py

--- sesamesyrup.py
+++ sesamesyrup.py
@@ -1,20 +1,23 @@
 import time
-from threading import Thread, Lock
-import sys
+import queue
+import threading
 
-lock = Lock()
-
-def animate_text(text, delay=0.1):
-    with lock:
-        for char in text:
-            sys.stdout.write(char)
-            sys.stdout.flush()
+def animate_text(text_queue, delay=0.1):
+    while True:
+        try:
+            char = text_queue.get(timeout=0.01)
+            if char is None:
+                break
+            print(char, end='', flush=True)
             time.sleep(delay)
-        print()
+        except queue.Empty:
+            pass
 
-def sing_lyric(lyric, delay, speed):
+def sing_lyric(lyric, delay, speed, text_queue):
     time.sleep(delay)
-    animate_text(lyric, speed)
+    for char in lyric:
+        text_queue.put(char)
+    text_queue.put(None)  # Signal end of lyric
 
 def sing_song():
     lyrics = [
@@ -26,16 +29,21 @@ def sing_song():
         ("I heard it a long time ago", 0.1)
     ]
     delays = [0.3, 5.0, 10.0, 15.0, 20.3, 25.0]
-    
+
+    text_queue = queue.Queue()
+    animator = threading.Thread(target=animate_text, args=(text_queue,))
+    animator.start()
+
     threads = []
     for i in range(len(lyrics)):
         lyric, speed = lyrics[i]
-        t = Thread(target=sing_lyric, args=(lyric, delays[i], speed))
+        t = threading.Thread(target=sing_lyric, args=(lyric, delays[i], speed, text_queue))
         threads.append(t)
         t.start()
-    
+
     for thread in threads:
         thread.join()
+    animator.join()
 
 if __name__ == "__main__":
     sing_song()
