cae255bc93ae9c901eb6daf63ee5f2d7a7623e0f
jugwave <bernadettegopela.c@gmail.com>
2024-08-20 02:47:18 +0800

    Update sesamesyrup.py

--- sesamesyrup.py
+++ sesamesyrup.py
@@ -1,49 +1,28 @@
 import time
-import queue
-import threading
+from threading import Thread, Lock
+import sys
 
-def animate_text(text_queue, delay=0.1):
-    while True:
-        try:
-            char = text_queue.get(timeout=0.01)
-            if char is None:
-                break
-            print(char, end='', flush=True)
+lock = Lock()
+
+def animate_text(text, delay=0.1):
+    with lock:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
             time.sleep(delay)
-        except queue.Empty:
-            pass
+        print()
 
-def sing_lyric(lyric, delay, speed, text_queue):
+def sing_lyric(lyric, delay, speed):
     time.sleep(delay)
-    for char in lyric:
-        text_queue.put(char)
-    text_queue.put(None)  # Signal end of lyric
+    animate_text(lyric, speed)
 
 def sing_song():
     lyrics = [
         ("You've had a long list of lovers,", 0.1),
-        ("But none of them matter to you except me", 0.1),
+        ("But none of them matter to you except me", 0.12),
         ("I've had a long list of lovers,", 0.1),
-        ("But none of them matter to me except you", 0.1),
-        ("Sesame syrup,", 0.2),
+        ("But none of them matter to me except you", 0.12),
+        ("Sesame syrup,", 0.08),
         ("I heard it a long time ago", 0.1)
     ]
-    delays = [0.3, 5.0, 10.0, 15.0, 20.3, 25.0]
-
-    text_queue = queue.Queue()
-    animator = threading.Thread(target=animate_text, args=(text_queue,))
-    animator.start()
-
-    threads = []
-    for i in range(len(lyrics)):
-        lyric, speed = lyrics[i]
-        t = threading.Thread(target=sing_lyric, args=(lyric, delays[i], speed, text_queue))
-        threads.append(t)
-        t.start()
-
-    for thread in threads:
-        thread.join()
-    animator.join()
-
-if __name__ == "__main__":
-    sing_song()
+    delays = [0.3, 5.0, 10.0, 14.5, 20.0, 24.
